86563f0543ced2bca52bd6539874cca235e825e3
Rob <rweyant@powerley.com>
2017-11-04 11:09:58 -0400

    remove unnecessary glue

--- R/albums.R
+++ R/albums.R
@@ -26,7 +26,7 @@ get_album <- function(id,...){
 #' get_albums(ids = c('41MnTivkwTO3UUJ8DrqEJJ', '6JWc4iAiJ9FjyK0B59ABb4', '6UXCm6bOO4gFlDQZV5yL37'), market='US')
 get_albums <- function(ids,...){
   query <- list(ids = paste(ids, collapse=','), ...)
-  response <- GET(url = glue('{ALBUMS_URL}'),
+  response <- GET(url = ALBUMS_URL,
                 add_headers(Authorization=paste('Bearer', access_token)),
                 query = query)
   get_response_content(response)
