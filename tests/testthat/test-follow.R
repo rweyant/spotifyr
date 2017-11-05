e829b9184b58ce083bc700a5fe93e5e2c0e8c39d
Rob <rweyant@powerley.com>
2017-11-05 17:10:58 -0500

    add basic tests for follow functions

---
+++ tests/testthat/test-follow.R
@@ -0,0 +1,15 @@
+context("Follow endpoints")
+
+test_that("follow functions return expected content", {
+
+  expected_album_fields <- c()
+
+  # setup
+  set_tokens()
+  # user_auth() # need to figure out a way to do this with cached
+
+  # response <- get_user_followed_artists()
+  # expect_is(typeof(response), 'character')
+
+})
+
