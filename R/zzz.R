156647c046bd4e2bd5cc2dfe0b6473fc1565f920
Rob <rweyant@powerley.com>
2017-11-05 17:10:12 -0500

    whitespace

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,9 @@
 .onLoad <- function(libname, pkgname){
+
   if(Sys.getenv('SPOTIFY_CLIENT') == '')
     warning('No Spotify client id found.  You need to set credentials as SPOTIFY_CLIENT environment variables in your ~/.Renviron file ')
+
   if(Sys.getenv('SPOTIFY_SECRET') == '')
     warning('No Spotify client id found.  You need to set credentials as SPOTIFY_SECRET environment variable in your ~/.Renviron file ')
+
 }
