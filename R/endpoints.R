dd385babbfc252a97a6eb69e14ea5da418342850
Rob <rweyant@powerley.com>
2017-11-05 17:08:14 -0500

    add additional endpoint constants

--- R/endpoints.R
+++ R/endpoints.R
@@ -2,19 +2,23 @@
 BASE_URL <- 'https://api.spotify.com'
 AUTHORIZE_URL <- 'https://accounts.spotify.com/authorize'
 TOKENS_URL <- 'https://accounts.spotify.com/api/token'
+API_VERSION <- 'v1'
 
-SEARCH_URL <- glue('{BASE_URL}/v1/search')
-ALBUMS_URL <- glue('{BASE_URL}/v1/albums')
-ARTIST_URL <- glue('{BASE_URL}/v1/artists')
-USER_URL <- glue('{BASE_URL}/v1/users')
+SEARCH_URL <- glue('{BASE_URL}/{API_VERSION}/search')
+ALBUMS_URL <- glue('{BASE_URL}/{API_VERSION}/albums')
+ARTIST_URL <- glue('{BASE_URL}/{API_VERSION}/artists')
+USER_URL <- glue('{BASE_URL}/{API_VERSION}/users')
+TRACKS_URL <- glue('{BASE_URL}/{API_VERSION}/tracks')
+AUDIO_ANALYSIS_URL <- glue('{BASE_URL}/{API_VERSION}/audio-analysis')
+AUDIO_FEATURES_URL <- glue('{BASE_URL}/{API_VERSION}/audio-features')
 
-BROWSE_FEATURED_PLAYLISTS_URL <- paste(BASE_URL,'/v1/browse/featured-playlists',sep='')
-BROWSE_NEW_RELEASES_URL<- paste(BASE_URL,'/v1/browse/new-releases',sep='')
-BROWSE_CATEGORIES_URL <- paste(BASE_URL,'/v1/browse/categories',sep='')
+BROWSE_FEATURED_PLAYLISTS_URL <- glue('{BASE_URL}/{API_VERSION}/browse/featured-playlists')
+BROWSE_NEW_RELEASES_URL <- glue('{BASE_URL}/{API_VERSION}/browse/new-releases')
+BROWSE_CATEGORIES_URL <- glue('{BASE_URL}/{API_VERSION}/browse/categories')
 
-FOLLOWING_URL <- 'https://api.spotify.com/v1/me/following'
-LIBRARY_URL <- 'https://api.spotify.com/v1/me/tracks'
-USER_URL <- 'https://api.spotify.com/v1/users'
+FOLLOWING_URL <- glue('{BASE_URL}/{API_VERSION}/me/following')
+LIBRARY_URL <- glue('{BASE_URL}/{API_VERSION}/me/tracks')
+USER_URL <- glue('{BASE_URL}/{API_VERSION}/users')
 
 ALL_SCOPES <- paste('playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private',
                     'streaming user-follow-modify user-follow-read user-library-modify user-library-read user-read-private',
