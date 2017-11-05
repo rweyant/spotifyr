22ab587470dcbf6298e39a6e71d30285aefaf64e
Rob <rweyant@powerley.com>
2017-11-05 14:17:20 -0500

    add oauth functions using pure httr

---
+++ R/auth2.R
@@ -0,0 +1,29 @@
+user_auth <- function(cache = TRUE) {
+
+  set_tokens()
+  response <- GET(url = AUTHORIZE_URL,
+                  query = list(client_id = Sys.getenv('SPOTIFY_CLIENT'),
+                               response_type = 'code',
+                               scope = ALL_SCOPES,
+                               redirect_uri = 'https://github.com/rweyant/spotifyr'))
+
+  # set dropbox oauth2 endpoints
+  spotify <- httr::oauth_endpoint(
+    authorize = AUTHORIZE_URL,
+    access = TOKENS_URL
+  )
+
+  # registered dropbox app's key & secret
+  spotify_app <- httr::oauth_app("spotifyr",
+                                 Sys.getenv('SPOTIFY_CLIENT'),
+                                 Sys.getenv('SPOTIFY_SECRET'))
+
+  # get the token
+  spotify_token <- httr::oauth2.0_token(
+    endpoint = spotify,
+    app = spotify_app,
+    scope = ALL_SCOPES,
+    cache = cache)
+  spotify_token
+  assign('user_token', spotify_token, envir = .GlobalEnv)
+}
